a2c3af3dba1026c996a126ba87e71df14a6d37fb
AndreaPoguisch <andreapoguisch.16@gmail.com>
2022-02-08 14:15:30 +0100

    Es8

--- .github/SSVSSA/SSVSSA/Program.cs
+++ .github/SSVSSA/SSVSSA/Program.cs
@@ -12,14 +12,14 @@ namespace SSVSSA
     public class Program
     {
         public static void Main(string[] args)
-        {
+        {cfb
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
-                {
+                }
                     webBuilder.UseStartup<Startup>();
                 });
     }
